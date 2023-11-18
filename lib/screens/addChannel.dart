ba836d561e58255780bea04519a0fa1279aae371
Insan Jeyakkumar <insan20011227@gmail.com>
2023-11-19 00:33:51 +0530

    initial app release code

--- lib/screens/addChannel.dart
+++ lib/screens/addChannel.dart
@@ -18,11 +18,12 @@ class _AddChannelScreenState extends State<AddChannelScreen> {
   TextEditingController controller = TextEditingController();
   late List<String> channelIdList;
   bool isProgress = false;
-  bool adding = false;
+
   bool validUrl = false;
   String hintText = "Add video url from your desired channel";
 
   List<Channel> newChannelList = [];
+  List<bool> newChannelProgress = [];
 
   searchChannel() {
     String userInput = controller.text;
@@ -40,6 +41,7 @@ class _AddChannelScreenState extends State<AddChannelScreen> {
       });
     } else {
       getChannelID(userInput).then((channel) {
+        newChannelProgress.insert(0, false);
         newChannelList.insert(0, channel);
         setState(() {
           hintText = "Add video url from your desired channel";
@@ -86,63 +88,65 @@ class _AddChannelScreenState extends State<AddChannelScreen> {
         Padding(
           padding: const EdgeInsets.only(top: 150, left: 20, right: 20),
           child: ListView.builder(
-            itemCount: newChannelList.length,
-            itemBuilder: (context, index) => Card(
-              color: Colors.grey,
-              child: ListTile(
-                leading: CircleAvatar(
-                  maxRadius: 30,
-                  foregroundImage: CachedNetworkImageProvider(
-                    newChannelList[index].logoUrl,
-                  ),
-                ),
-                title: Text(newChannelList[index].title),
-                subtitle: Text(
-                    "${newChannelList[index].subscribersCount} Subscribers"),
-                trailing: adding
-                    ? const CircularProgressIndicator()
-                    : IconButton(
-                        onPressed: () {
-                          setState(() {
-                            adding = true;
-                          });
-                          if (prefs.getStringList(prefKey)?.contains(
-                                  newChannelList[index].id.toString()) ??
-                              false) {
-                            ScaffoldMessenger.of(context).showSnackBar(
-                                const SnackBar(
-                                    content:
-                                        Text("Already added this channel")));
-                            setState(() {
-                              isProgress = false;
-                            });
-                          } else {
-                            getAllChannelInfo(
-                                    [newChannelList[index].id.toString()],
-                                    false,
-                                    {})
-                                .then((value) => context
-                                    .read<ValueProvider>()
-                                    .addValue(value[0]))
-                                .then((value) {
-                              ScaffoldMessenger.of(context).showSnackBar(
-                                const SnackBar(
-                                  content: Text("Channel added successfully"),
-                                ),
-                              );
-                              newChannelList.remove(newChannelList[index]);
-                              setState(() {
-                                newChannelList;
-                                adding = false;
-                              });
-                            });
-                          }
-                        },
-                        icon: const Icon(Icons.add),
+              itemCount: newChannelList.length,
+              itemBuilder: (context, index) {
+                return Card(
+                  color: Colors.grey,
+                  child: ListTile(
+                    leading: CircleAvatar(
+                      maxRadius: 30,
+                      foregroundImage: CachedNetworkImageProvider(
+                        newChannelList[index].logoUrl,
                       ),
-              ),
-            ),
-          ),
+                    ),
+                    title: Text(newChannelList[index].title),
+                    subtitle: Text(
+                        "${newChannelList[index].subscribersCount} Subscribers"),
+                    trailing: newChannelProgress[index]
+                        ? const CircularProgressIndicator()
+                        : IconButton(
+                            onPressed: () {
+                              setState(() {
+                                newChannelProgress[index] = true;
+                              });
+                              if (prefs.getStringList(prefKey)?.contains(
+                                      newChannelList[index].id.toString()) ??
+                                  false) {
+                                ScaffoldMessenger.of(context).showSnackBar(
+                                    const SnackBar(
+                                        content: Text(
+                                            "Already added this channel")));
+                                setState(() {
+                                  isProgress = false;
+                                });
+                              } else {
+                                getAllChannelInfo(
+                                        [newChannelList[index].id.toString()],
+                                        false,
+                                        {})
+                                    .then((value) => context
+                                        .read<ValueProvider>()
+                                        .addValue(value[0]))
+                                    .then((value) {
+                                  ScaffoldMessenger.of(context).showSnackBar(
+                                    const SnackBar(
+                                      content:
+                                          Text("Channel added successfully"),
+                                    ),
+                                  );
+                                  newChannelList.remove(newChannelList[index]);
+                                  setState(() {
+                                    newChannelList;
+                                    newChannelProgress[index] = false;
+                                  });
+                                });
+                              }
+                            },
+                            icon: const Icon(Icons.add),
+                          ),
+                  ),
+                );
+              }),
         )
       ],
     );
